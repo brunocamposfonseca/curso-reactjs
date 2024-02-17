853f65fecfb12e452f69be5b2ec8e5e97a993ebf
Bruno Campos Fonseca <92186023+brunocamposfonseca@users.noreply.github.com>
2024-02-16 21:31:14 -0300

    react.0.1

--- src/components/Objects/Carrossel/index.js
+++ src/components/Objects/Carrossel/index.js
@@ -1,15 +1,17 @@
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style.css";
 
 function Carroussel({ children }) {
-
     const settings = {
         dots: false,
         infinite: false,
-        speed: 300,
+        speed: 600,
         centerMode: false,
         variableWidth: true,
         adaptiveHeight: true,
-        //slidesToShow: 3,
+        //slidesToShow: 4,
         slidesToScroll: 1,
     };
 
